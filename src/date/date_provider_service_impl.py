9dc115ad8237cf6a77f80f4fee120e5cc950e3f4
jgithub <jgithub1@jeano.net>
2023-11-11 13:41:35 -0600

    dev

--- src/date/date_provider_service_impl.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from datetime import datetime
-
-from date_provider_service import DateProviderService
-
-class DateProviderServiceImpl(DateProviderService):
-  def get_now(self):
-      return datetime.now()
--- src/greeting/greeting_provider_service.py
+++
@@ -1,6 +0,0 @@
-from abc import ABC, abstractmethod
-
-class GreetingProviderService(ABC):
-  @abstractmethod
-  def get_greeting(self) -> str:
-      pass
