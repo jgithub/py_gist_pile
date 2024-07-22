3731ab81a217f762d173c3312b31fcc7d4449c00
jgithub <jgithub1@jeano.net>
2024-07-21 23:36:45 -0500

    towards ts-gist-pile parity

--- py_gist_pile/llm/llm_query.py
+++ py_gist_pile/llm/llm_query.py
@@ -5,8 +5,12 @@ from llm.llm_q_and_a import LlmQAndA
 from dataclasses import dataclass
 
 
+
 @dataclass
 class LlmQuery:
+    '''
+    Anemic LlmQuery model
+    '''
     prompt: str
     additionalContext: JsonObjectOrArray
     qAndA: List[LlmQAndA]
