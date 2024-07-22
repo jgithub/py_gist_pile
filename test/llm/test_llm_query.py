05c5f33949b4ae4a9b584c09aa83253cb8e3d48e
jgithub <jgithub1@jeano.net>
2024-07-22 06:04:07 -0500

    towards ts-gist-pile parity

---
+++ test/llm/test_llm_query.py
@@ -0,0 +1,12 @@
+import unittest
+from boolean_util import is_truelike
+from llm_query import LlmQuery
+
+
+class TestLlmQuery(unittest.TestCase):
+    def test_anemic(self):
+        llm_query = LlmQuery()
+
+
+if __name__ == "__main__":
+    unittest.main()
